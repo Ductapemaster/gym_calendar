fc165de57f680700e9b1dcc8f4677617ee5c2c76
Dan Kouba <ductapemaster@gmail.com>
2019-01-07 09:50:42 -0800

    Add example secrets file

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# This file contains information you may not want to make public,
+# PLEASE DO NOT COMMIT IT TO A PUBLIC REPOSITORY AFTER YOU ADD YOUR INFORMATION TO IT.
+
+# Calendars used in the main program
+# Fill these in with your desired calendar IDs.  These can be found in the
+# calendar settings inside the Google Calendar web app
+goleta_calendar_id = "example1@group.calendar.google.com"
+uptown_calendar_id = "example2@group.calendar.google.com"
+downtown_calendar_id = "example3@group.calendar.google.com"
+
+# List of email addresses to invite to the created events
+invite_addresses = ["example@example.com"]
