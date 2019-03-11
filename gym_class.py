7b3839f5477b680bc8f7b9d36bbd2b506ddfdc9a
Dan Kouba <ductpaemaster@gmail.com>
2019-03-10 23:39:32 -0700

    Refactor gym_class to use the classmethod decorator
    Just playing around with @classmethod

--- gym_class.py
+++ gym_class.py
@@ -6,17 +6,26 @@ import config
 logger_gym_class = logging.getLogger("gym_class")
 
 
-
 # TODO: split into a parent class that doesn't include the calendar event specific code and a child that does
 class GymClass:
-    def __init__(self, gym, json_string):
+    def __init__(self, class_name="", instructor_name="",
+                 start_time=datetime.now(), end_time=datetime.now(),
+                 gym=None, id_seed=" "):
+        self.class_name = class_name
+        self.instructor_name = instructor_name
+        self.start_time = start_time
+        self.end_time = end_time
+        self.duration = self.end_time - self.start_time
+        self.gym = gym
+        self.hash = sha3_256(id_seed.encode('utf-8')).hexdigest()
+
+    @classmethod
+    def from_json(cls, gym, json_string):
         try:
-            self.class_name = json_string["gym_class"]["name"]
-            self.instructor_name = json_string["instructor"]["display_name"]
-            self.start_time = datetime.strptime(json_string["next_occurrence"]["open"]["date"], "%Y-%m-%d %H:%M:%S.%f")
-            self.end_time = datetime.strptime(json_string["next_occurrence"]["close"]["date"], "%Y-%m-%d %H:%M:%S.%f")
-            self.duration = self.end_time - self.start_time
-            self.gym = gym
+            class_name = json_string["gym_class"]["name"]
+            instructor_name = json_string["instructor"]["display_name"]
+            start_time = datetime.strptime(json_string["next_occurrence"]["open"]["date"], "%Y-%m-%d %H:%M:%S.%f")
+            end_time = datetime.strptime(json_string["next_occurrence"]["close"]["date"], "%Y-%m-%d %H:%M:%S.%f")
 
             # Generate a unique ID that can be used for the event ID on the calendar
             class_id = json_string["class_id"]
@@ -25,15 +34,21 @@ class GymClass:
             start_date_string = json_string["next_occurrence"]["open"]["date"]
             end_date_string = json_string["next_occurrence"]["close"]["date"]
             unique_id = str(class_id) + str(gym_id) + str(instructor_id) + start_date_string + end_date_string
-            self.hash = sha3_256(unique_id.encode('utf-8')).hexdigest()
 
-        except Exception as e:
-            self.class_name = ""
-            self.instructor_name = ""
-            self.start_time = None
-            self.end_time = None
-            self.duration = None
+        except Exception:
+            class_name = ""
+            instructor_name = ""
+            start_time = datetime.now()
+            end_time = datetime.now()
+            unique_id = " "
             logger_gym_class.error("Error parsing JSON into class", exc_info=True)
+
+        return cls(class_name=class_name,
+                   instructor_name=instructor_name,
+                   start_time=start_time,
+                   end_time=end_time,
+                   gym=gym,
+                   id_seed=unique_id)
 
     def event_object(self, attendees=[]):
         return {
